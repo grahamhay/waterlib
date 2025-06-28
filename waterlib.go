fa5010eeca0933ece36e4f37d15823e17118d2d2
Graham Hay <graham.hay@xtra.co.nz>
2025-06-28 14:45:39 +1200

    updated mod

--- waterlib.go
+++ waterlib.go
@@ -1,6 +1,14 @@
 package waterlib
 
-func CalculateWaterConsumption(FromReading string, UntilReading string) string {
+import (
+	"fmt"
+	"strings"
+)
 
-	return "0,000"
+func CalculateWaterConsumption(FromReading string, UntilReading string) string {
+	// TODO Validate the inputs
+
+	myArgs := strings.Split(FromReading, "-")
+	fmt.Println(myArgs[0], myArgs[1])
+	return "00000,000"
 }
